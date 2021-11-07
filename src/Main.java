78211dc177709fe2df5bfc8bf780b7c0a2ba68d9
Krzysztof Andruszkiewicz <kruk7kruk@gmail.com>
2021-11-07 12:58:17 +0100

    Hello World my first progrram

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        int liczba, liczba1;
-        liczba = 11;
-        liczba1= 2;
+        int liczba = 2, liczba1 = 11;
 
         System.out.println("Hello world " + liczba);
-        System.out.println("raz, dwa trzy");
 
+        System.out.println("raz, dwa trzy");
 
         System.out.println(liczba * liczba1);
 
+        String imie = "Kałakutas";
+        int wiek = 37;
+
+        System.out.println("mam na imie " + imie + " mam " + wiek  + " lat");
 
     }
 
