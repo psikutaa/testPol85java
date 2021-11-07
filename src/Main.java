0b7a5687fcd56d2c32ba72615eae8b565bd7a0c2
Krzysztof Andruszkiewicz <kruk7kruk@gmail.com>
2021-11-07 14:31:54 +0100

    Hello World my first progrram

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-//        int liczba = 2, liczba1 = 11;
+        int liczba = 1, liczba1 = 1;
 //
 //        System.out.println("Hello world " + liczba);
 //
@@ -25,18 +25,26 @@ public class Main {
 //        System.out.println(account.trim());
 //
 //        System.out.println(account.length());
+//
+//        char a = 'a';
+//
+//        char alfa = '\u0381';
+//
+//
+//        System.out.println(alfa);
+//
+//        char[] powitanie = {'w', 'i', 't', 'a', 'j'};
+//        System.out.println(powitanie);
+//        System.out.println(Character.isDigit(a));
 
-        char a = 'a';
-
-        char alfa = '\u0381';
-
-
-        System.out.println(alfa);
-
-        char[] powitanie = {'w', 'i', 't', 'a', 'j'};
-        System.out.println(powitanie);
-        System.out.println(Character.isDigit(a));
-
+//        if (liczba1 > liczba) {
+//            System.out.println("liczba1 jest wieksza od liczba");
+//        } else {
+//            System.out.println("jest na odwrot");
+//            if (liczba1 == liczba) {
+//                System.out.println("są  takie same");
+//            }
+//        }
 
     }
 
